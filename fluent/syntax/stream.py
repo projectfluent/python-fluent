ddb1b29e3764d0f5e6059160bafe1ab73be44923
Zibi Braniecki <gandalf@mozilla.com>
2017-02-07 18:19:18 -0800

    Finish ParserStream

--- fluent/syntax/stream.py
+++ fluent/syntax/stream.py
@@ -15,7 +15,7 @@ class ParserStream():
     def __init__(self, string):
         self.iter = StringIter(string)
         self.buf = []
-        self.peekIndex = 0
+        self.peek_index = 0
         self.index = 0
 
         self.ch = None
@@ -32,7 +32,7 @@ class ParserStream():
         if len(self.buf) == 0:
             self.ch = self.iter.next()
         else:
-            self.ch = self.buf.shift()
+            self.ch = self.buf.pop(0)
 
         self.index += 1
 
@@ -47,6 +47,64 @@ class ParserStream():
     def current(self):
         return self.ch
 
+    def current_is(self, ch):
+        return self.ch == ch
+
+    def current_peek(self):
+        if self.peek_end:
+            return None
+
+        diff = self.peek_index - self.index
+
+        if diff == 0:
+            return self.ch
+        return self.buf[diff - 1]
+
+    def current_peek_is(self, ch):
+        return self.current_peek() == ch
+
+    def peek(self):
+        if self.peek_end:
+            return None
+
+        self.peek_index += 1
+
+        diff = self.peek_index - self.index
+
+        if diff > len(self.buf):
+            ch = self.iter.next()
+            if ch is not None:
+                self.buf.append(ch)
+            else:
+                self.peek_end = True
+                return None
+
+        return self.buf[diff - 1]
+
     def get_index(self):
         return self.index
 
+    def get_peek_index(self):
+        return self.peek_index
+
+    def peek_char_is(self, ch):
+        if self.peek_end:
+            return False
+
+        ret = self.peek()
+
+        self.peek_index -= 1
+
+        return ret == ch
+
+    def reset_peek(self):
+        self.peek_index = self.index
+        self.peek_end = self.iter_end
+
+    def skip_to_peek(self):
+        diff = self.peek_index - self.index
+
+        for i in range(0, diff):
+            self.ch = self.buf.pop(0)
+
+        self.index = self.peek_index

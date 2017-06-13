b7ef37c7f12f3471d49dd3ef9f0026ef12849569
Staś Małolepszy <stas@mozilla.com>
2017-06-13 11:50:59 +0200

    fluent 0.4.0

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# Changelog
+
+## Unreleased
+
+  - …
+
+## fluent 0.4.0 (June 13th, 2017)
+
+  - This is the first release to be listed in the CHANGELOG.
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='fluent',
-      version='0.3',
+      version='0.4.0',
       description='Localization library for expressive translations.',
       author='Mozilla',
       author_email='l10n-drivers@mozilla.org',

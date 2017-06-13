367828e5c7d89c71eb7df887d163b97bcb34b9fd
Staś Małolepszy <stas@mozilla.com>
2017-06-13 10:22:53 +0200

    Update package metainformation

--- setup.py
+++ setup.py
@@ -4,21 +4,21 @@ from setuptools import setup
 
 setup(name='fluent',
       version='0.3',
-      description='Python fluent library',
-      author='Staś Małolepszy',
-      author_email='stas@mozilla.com',
+      description='Localization library for expressive translations.',
+      author='Mozilla',
+      author_email='l10n-drivers@mozilla.org',
       license='APL 2',
       url='https://github.com/projectfluent/python-fluent',
+      keywords=['fluent', 'localization', 'l10n'],
       classifiers=[
-            'Development Status :: 3 - Alpha',
-            'Intended Audience :: Developers',
-            'License :: OSI Approved :: Apache Software License',
-            'Programming Language :: Python :: 2.6',
-            'Programming Language :: Python :: 2.7',
-            'Programming Language :: Python :: 3.5',
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Apache Software License',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.5',
       ],
-      packages=['fluent', 'fluent.syntax'],
-      install_requires=[
-          'six'
-      ]
+      packages=['fluent', 'fluent.syntax', 'fluent.migrate'],
+      package_data={
+          'fluent.migrate': ['cldr_data/*']
+      }
       )

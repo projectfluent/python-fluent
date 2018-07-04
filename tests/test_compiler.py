797aea3d7ba1ea2cbb9338122cf596467569897c
Luke Plant <L.Plant.98@cantab.net>
2018-07-04 10:19:49 +0100

    tests: small refactor

--- tests/test_compiler.py
+++ tests/test_compiler.py
@@ -29,7 +29,7 @@ def parse_ftl(source):
 
 
 def compile_messages_to_python(source, locale, use_isolating=True, strict=True):
-    messages = parse_ftl(source)
+    messages = parse_ftl(dedent_ftl(source))
     module, message_mapping, module_globals = messages_to_module(messages, locale,
                                                                  use_isolating=use_isolating,
                                                                  strict=strict)
@@ -46,28 +46,28 @@ class TestCompiler(unittest.TestCase):
                          normalize_python(code2))
 
     def test_single_string_literal(self):
-        code = compile_messages_to_python(dedent_ftl("""
+        code = compile_messages_to_python("""
             foo = Foo
-        """), self.locale)
+        """, self.locale)
         self.assertCodeEqual(code, """
             def foo(message_args, locale, errors):
                 return ('Foo', errors)
         """)
 
     def test_string_literal_in_placeable(self):
-        code = compile_messages_to_python(dedent_ftl("""
+        code = compile_messages_to_python("""
             foo = { "Foo" }
-        """), self.locale)
+        """, self.locale)
         self.assertCodeEqual(code, """
         def foo(message_args, locale, errors):
             return ('Foo', errors)
         """)
 
     def test_message_reference_plus_string_literal(self):
-        code = compile_messages_to_python(dedent_ftl("""
+        code = compile_messages_to_python("""
             foo = Foo
             bar = X { foo }
-        """), self.locale)
+        """, self.locale)
         self.assertCodeEqual(code, """
             def foo(message_args, locale, errors):
                 return ('Foo', errors)
@@ -78,10 +78,10 @@ class TestCompiler(unittest.TestCase):
         """)
 
     def test_single_message_reference(self):
-        code = compile_messages_to_python(dedent_ftl("""
+        code = compile_messages_to_python("""
             foo = Foo
             bar = { foo }
-        """), self.locale)
+        """, self.locale)
         self.assertCodeEqual(code, """
             def foo(message_args, locale, errors):
                 return ('Foo', errors)
@@ -92,10 +92,10 @@ class TestCompiler(unittest.TestCase):
 
     def test_single_message_reference_reversed_order(self):
         # We should cope with forward references
-        code = compile_messages_to_python(dedent_ftl("""
+        code = compile_messages_to_python("""
             bar = { foo }
             foo = Foo
-        """), self.locale)
+        """, self.locale)
         self.assertCodeEqual(code, """
             def bar(message_args, locale, errors):
                 return foo(message_args, locale, errors)
@@ -105,9 +105,9 @@ class TestCompiler(unittest.TestCase):
         """)
 
     def test_single_message_bad_reference(self):
-        code = compile_messages_to_python(dedent_ftl("""
+        code = compile_messages_to_python("""
             bar = { foo }
-        """), self.locale)
+        """, self.locale)
         # We already know that foo does not exist, so we can hard code the error
         # into the function.
         self.assertCodeEqual(code, """
@@ -117,18 +117,18 @@ class TestCompiler(unittest.TestCase):
         """)
 
     def test_name_collision_function_args(self):
-        code = compile_messages_to_python(dedent_ftl("""
+        code = compile_messages_to_python("""
             errors = Errors
-        """), self.locale)
+        """, self.locale)
         self.assertCodeEqual(code, """
             def errors2(message_args, locale, errors):
                 return ('Errors', errors)
         """)
 
     def test_name_collision_builtins(self):
-        code = compile_messages_to_python(dedent_ftl("""
+        code = compile_messages_to_python("""
             zip = Zip
-        """), self.locale)
+        """, self.locale)
         self.assertCodeEqual(code, """
             def zip2(message_args, locale, errors):
                 return ('Zip', errors)
@@ -137,10 +137,10 @@ class TestCompiler(unittest.TestCase):
     def test_message_mapping_used(self):
         # Checking that we actually use message_mapping when looking up the name
         # of the message function to call.
-        code = compile_messages_to_python(dedent_ftl("""
+        code = compile_messages_to_python("""
             zip = Foo
             str = { zip }
-        """), self.locale)
+        """, self.locale)
         self.assertCodeEqual(code, """
             def zip2(message_args, locale, errors):
                 return ('Foo', errors)
@@ -150,9 +150,9 @@ class TestCompiler(unittest.TestCase):
         """)
 
     def test_external_argument(self):
-        code = compile_messages_to_python(dedent_ftl("""
+        code = compile_messages_to_python("""
             with-arg = { $arg }
-        """), self.locale)
+        """, self.locale)
         self.assertCodeEqual(code, """
             def with_arg(message_args, locale, errors):
                 try:

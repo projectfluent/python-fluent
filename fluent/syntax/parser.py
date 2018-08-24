6a6683fd854b47bf93a47dfe18e3ae224a2541e1
Axel Hecht <axel@pike.org>
2018-08-24 16:14:51 +0200

    remove most of the old code to get a cleaner start, copied some to CompatFluentParser

--- fluent/syntax/parser.py
+++ fluent/syntax/parser.py
@@ -69,19 +69,6 @@ class FluentParser(object):
         rv = self.get_blank_line(cursor)
         if rv is not None:
             return rv
-        # error_index = ps.get_index()
-        # ps.skip_to_next_entry_start()
-        # next_entry_start = ps.get_index()
-
-        # # Create a Junk instance
-        # slice = ps.get_slice(entry_start_pos, next_entry_start)
-        # junk = ast.Junk(slice)
-        # if self.with_spans:
-        #     junk.add_span(entry_start_pos, next_entry_start)
-        # annot = ast.Annotation(err.code, err.args, err.message)
-        # annot.add_span(error_index, error_index)
-        # junk.add_annotation(annot)
-        # return junk
 
     def get_entry(self, cursor):
         rv = self.get_message(cursor)
@@ -110,40 +97,6 @@ class FluentParser(object):
         '''
         m = RE.blank_line.match(self.source, cursor)
         return None if m is None else (None, m.end())
-
-    @with_span
-    def get_zero_four_style_comment(self, ps):
-        ps.expect_char('/')
-        ps.expect_char('/')
-        ps.take_char(lambda x: x == ' ')
-
-        content = ''
-
-        while True:
-            ch = ps.take_char(lambda x: x != '\n')
-            while ch:
-                content += ch
-                ch = ps.take_char(lambda x: x != '\n')
-
-            if ps.is_peek_next_line_zero_four_style_comment():
-                content += ps.current()
-                ps.next()
-                ps.expect_char('/')
-                ps.expect_char('/')
-                ps.take_char(lambda x: x == ' ')
-            else:
-                break
-
-        # Comments followed by Sections become GroupComments.
-        ps.peek()
-        if ps.current_peek_is('['):
-            ps.skip_to_peek()
-            self.get_group_comment_from_section(ps)
-            return ast.GroupComment(content)
-
-        ps.reset_peek()
-        ps.last_comment_zero_four_syntax = True
-        return ast.Comment(content)
 
     @with_span
     def get_comment(self, cursor):
@@ -177,24 +130,6 @@ class FluentParser(object):
         # Filter out None
         content = [seg for seg in content if seg is not None]
         return Node(''.join(content)), cursor
-
-    @with_span
-    def get_group_comment_from_section(self, ps):
-        ps.expect_char('[')
-        ps.expect_char('[')
-
-        ps.skip_inline_ws()
-
-        self.get_variant_name(ps)
-
-        ps.skip_inline_ws()
-
-        ps.expect_char(']')
-        ps.expect_char(']')
-
-        # A Section without a comment is like an empty Group Comment.
-        # Semantically it ends the previous group and starts a new one.
-        return ast.GroupComment('')
 
     @with_span
     def get_message(self, cursor):
@@ -232,26 +167,6 @@ class FluentParser(object):
         if cursor is None:
             return cursor
         return self.skip_blank_inline(cursor)
-
-    @with_span
-    def get_term(self, ps):
-        id = self.get_term_identifier(ps)
-
-        ps.skip_inline_ws()
-        ps.expect_char('=')
-
-        if ps.is_peek_value_start():
-            ps.skip_indent()
-            value = self.get_value(ps)
-        else:
-            raise ParseError('E0006', id.name)
-
-        if ps.is_peek_next_line_attribute_start():
-            attrs = self.get_attributes(ps)
-        else:
-            attrs = None
-
-        return ast.Term(id, value, attrs)
 
     @with_span
     def get_attribute(self, cursor):
@@ -321,123 +236,6 @@ class FluentParser(object):
         cursor = match.end()
         return ast.Identifier(name), cursor
 
-    def get_variant_key(self, ps):
-        ch = ps.current()
-
-        if ch is None:
-            raise ParseError('E0013')
-
-        cc = ord(ch)
-        if ((cc >= 48 and cc <= 57) or cc == 45):  # 0-9, -
-            return self.get_number(ps)
-
-        return self.get_variant_name(ps)
-
-    @with_span
-    def get_variant(self, ps, has_default):
-        default_index = False
-
-        if ps.current_is('*'):
-            if has_default:
-                raise ParseError('E0015')
-            ps.next()
-            default_index = True
-
-        ps.expect_char('[')
-
-        key = self.get_variant_key(ps)
-
-        ps.expect_char(']')
-
-        if ps.is_peek_value_start():
-            ps.skip_indent()
-            value = self.get_value(ps)
-            return ast.Variant(key, value, default_index)
-
-        raise ParseError('E0012')
-
-    def get_variants(self, ps):
-        variants = []
-        has_default = False
-
-        while True:
-            ps.expect_indent()
-            variant = self.get_variant(ps, has_default)
-
-            if variant.default:
-                has_default = True
-
-            variants.append(variant)
-
-            if not ps.is_peek_next_line_variant_start():
-                break
-
-        if not has_default:
-            raise ParseError('E0010')
-
-        return variants
-
-    @with_span
-    def get_variant_name(self, ps):
-        name = ps.take_id_start()
-        while True:
-            ch = ps.take_variant_name_char()
-            if ch:
-                name += ch
-            else:
-                break
-
-        return ast.VariantName(name.rstrip(' \t\n\r'))
-
-    def get_digits(self, ps):
-        num = ''
-
-        ch = ps.take_digit()
-        while ch:
-            num += ch
-            ch = ps.take_digit()
-
-        if len(num) == 0:
-            raise ParseError('E0004', '0-9')
-
-        return num
-
-    @with_span
-    def get_number(self, ps):
-        num = ''
-
-        if ps.current_is('-'):
-            num += '-'
-            ps.next()
-
-        num += self.get_digits(ps)
-
-        if ps.current_is('.'):
-            num += '.'
-            ps.next()
-            num += self.get_digits(ps)
-
-        return ast.NumberLiteral(num)
-
-    @with_span
-    def get_value(self, ps):
-        if ps.current_is('{'):
-            ps.peek()
-            ps.peek_inline_ws()
-            if ps.is_peek_next_line_variant_start():
-                return self.get_variant_list(ps)
-
-        return self.get_pattern(ps)
-
-    @with_span
-    def get_variant_list(self, ps):
-        ps.expect_char('{')
-        ps.skip_inline_ws()
-        variants = self.get_variants(ps)
-        ps.expect_indent()
-        ps.expect_char('}')
-        return ast.VariantList(variants)
-
     @with_span
     def get_pattern(self, cursor):
         elements = []
@@ -488,27 +286,6 @@ class FluentParser(object):
         # break_indent production.
         return ast.TextElement('\n'.join(buf)), cursor
 
-    def get_escape_sequence(self, ps, specials=('{', '\\')):
-        next = ps.current()
-
-        if next in specials:
-            ps.next()
-            return '\\{}'.format(next)
-
-        if next == 'u':
-            sequence = ''
-            ps.next()
-
-            for _ in range(4):
-                ch = ps.take_hex_digit()
-                if ch is None:
-                    raise ParseError('E0026', sequence + ps.current())
-                sequence += ch
-
-            return '\\u{}'.format(sequence)
-
-        raise ParseError('E0025', next)
-
     @with_span
     def get_placeable(self, cursor):
         cursor = self.require_char(cursor, '{')
@@ -546,161 +323,6 @@ class FluentParser(object):
         term_ident, cursor = rv
         return ast.TermReference(term_ident), cursor
 
-    @with_span
-    def get_selector_expression(self, ps):
-        if ps.current_is('{'):
-            return self.get_placeable(ps)
-
-        literal = self.get_literal(ps)
-
-        if not isinstance(literal, (ast.MessageReference, ast.TermReference)):
-            return literal
-
-        ch = ps.current()
-
-        if (ch == '.'):
-            ps.next()
-            attr = self.get_identifier(ps)
-            return ast.AttributeExpression(literal, attr)
-
-        if (ch == '['):
-            ps.next()
-
-            if isinstance(literal, ast.MessageReference):
-                raise ParseError('E0024')
-
-            key = self.get_variant_key(ps)
-            ps.expect_char(']')
-            return ast.VariantExpression(literal, key)
-
-        if (ch == '('):
-            ps.next()
-
-            if not re.match('^[A-Z][A-Z_?-]*$', literal.id.name):
-                raise ParseError('E0008')
-
-            positional, named = self.get_call_args(ps)
-            ps.expect_char(')')
-
-            func = ast.Function(literal.id.name)
-            if (self.with_spans):
-                func.add_span(literal.span.start, literal.span.end)
-
-            return ast.CallExpression(func, positional, named)
-
-        return literal
-
-    @with_span
-    def get_call_arg(self, ps):
-        exp = self.get_selector_expression(ps)
-
-        ps.skip_inline_ws()
-
-        if not ps.current_is(':'):
-            return exp
-
-        if not isinstance(exp, ast.MessageReference):
-            raise ParseError('E0009')
-
-        ps.next()
-        ps.skip_inline_ws()
-
-        val = self.get_arg_val(ps)
-
-        return ast.NamedArgument(exp.id, val)
-
-    def get_call_args(self, ps):
-        positional = []
-        named = []
-        argument_names = set()
-
-        ps.skip_inline_ws()
-        ps.skip_indent()
-
-        while True:
-            if ps.current_is(')'):
-                break
-
-            arg = self.get_call_arg(ps)
-            if isinstance(arg, ast.NamedArgument):
-                if arg.name.name in argument_names:
-                    raise ParseError('E0022')
-                named.append(arg)
-                argument_names.add(arg.name.name)
-            elif len(argument_names) > 0:
-                raise ParseError('E0021')
-            else:
-                positional.append(arg)
-
-            ps.skip_inline_ws()
-            ps.skip_indent()
-
-            if ps.current_is(','):
-                ps.next()
-                ps.skip_inline_ws()
-                ps.skip_indent()
-                continue
-            else:
-                break
-
-        return positional, named
-
-    def get_arg_val(self, ps):
-        if ps.is_number_start():
-            return self.get_number(ps)
-        elif ps.current_is('"'):
-            return self.get_string(ps)
-        raise ParseError('E0012')
-
-    @with_span
-    def get_string(self, ps):
-        val = ''
-
-        ps.expect_char('"')
-
-        ch = ps.take_char(lambda x: x != '"' and x != '\n')
-        while ch:
-            if ch == '\\':
-                val += self.get_escape_sequence(ps, ('{', '\\', '"'))
-            else:
-                val += ch
-            ch = ps.take_char(lambda x: x != '"' and x != '\n')
-
-        if ps.current_is('\n'):
-            raise ParseError('E0020')
-
-        ps.next()
-
-        return ast.StringLiteral(val)
-
-    @with_span
-    def get_literal(self, ps):
-        ch = ps.current()
-
-        if ch is None:
-            raise ParseError('E0014')
-
-        if ch == '$':
-            ps.next()
-            id = self.get_identifier(ps)
-            return ast.VariableReference(id)
-
-        elif ps.is_identifier_start():
-            id = self.get_identifier(ps)
-            return ast.MessageReference(id)
-
-        elif ps.is_number_start():
-            return self.get_number(ps)
-
-        elif ch == '-':
-            id = self.get_term_identifier(ps)
-            return ast.TermReference(id)
-
-        elif ch == '"':
-            return self.get_string(ps)
-
-        raise ParseError('E0014')
-
     def skip_blank_inline(self, cursor):
         m = RE.blank_inline.match(self.source, cursor)
         return cursor if m is None else m.end()
@@ -719,6 +341,71 @@ class FluentParser(object):
         if self.source[cursor] != char:
             return None
         return cursor + 1
+
+
+class CompatFluentParser(FluentParser):
+    def expect_equals(self, cursor):
+        '''Messages require an = after their ID.
+
+        Overwritten to disable that requirement.
+        '''
+        cursor = self.require_char(cursor, '=')
+        if cursor is None:
+            return None
+        return self.skip_blank_inline(cursor)
+
+    @with_span
+    def get_group_comment_from_section(self, cursor):
+        raise NotImplementedError
+        ps.expect_char('[')
+        ps.expect_char('[')
+
+        ps.skip_inline_ws()
+
+        self.get_variant_name(ps)
+
+        ps.skip_inline_ws()
+
+        ps.expect_char(']')
+        ps.expect_char(']')
+
+        # A Section without a comment is like an empty Group Comment.
+        # Semantically it ends the previous group and starts a new one.
+        return ast.GroupComment('')
+
+    @with_span
+    def get_zero_four_style_comment(self, cursor):
+        ps.expect_char('/')
+        ps.expect_char('/')
+        ps.take_char(lambda x: x == ' ')
+
+        content = ''
+
+        while True:
+            ch = ps.take_char(lambda x: x != '\n')
+            while ch:
+                content += ch
+                ch = ps.take_char(lambda x: x != '\n')
+
+            if ps.is_peek_next_line_zero_four_style_comment():
+                content += ps.current()
+                ps.next()
+                ps.expect_char('/')
+                ps.expect_char('/')
+                ps.take_char(lambda x: x == ' ')
+            else:
+                break
+
+        # Comments followed by Sections become GroupComments.
+        ps.peek()
+        if ps.current_peek_is('['):
+            ps.skip_to_peek()
+            self.get_group_comment_from_section(ps)
+            return ast.GroupComment(content)
+
+        ps.reset_peek()
+        ps.last_comment_zero_four_syntax = True
+        return ast.Comment(content)
 
 
 class PATTERNS(object):

1011e286f5b274d880d1ee03d19b1c2d105821f7
Luke Plant <L.Plant.98@cantab.net>
2019-01-24 16:49:35 +0300

    docs - enabled viewcode extension to work

--- fluent.runtime/docs/conf.py
+++ fluent.runtime/docs/conf.py
@@ -12,9 +12,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
 
 
 # -- Project information -----------------------------------------------------
@@ -24,7 +24,7 @@ copyright = '2019, Luke Plant'
 author = 'Luke Plant'
 
 # The short X.Y version
-version = ''
+version = '0.1'
 # The full version, including alpha/beta/rc tags
 release = '0.1'
 

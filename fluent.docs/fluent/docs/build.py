5281b513b1e00e1ffbffd31ef0109cfc395bfb87
Axel Hecht <axel@pike.org>
2020-12-25 19:58:52 +0100

    Refactor build code.
    Use classes for builders, use subclassing instead of code-flow
    ifs to distinguish the top-level builder from the project builders.
    
    This is in preparation for doing project builders per version.

--- fluent.docs/fluent/docs/build.py
+++ fluent.docs/fluent/docs/build.py
@@ -14,29 +14,106 @@ def get_version(root):
 
 def build(repo_name, doc_roots):
     for doc_root in doc_roots:
-        env = os.environ.copy()
         version = get_version(doc_root)
-        cmd = [
+        src_dir = doc_root
+        project = doc_root if doc_root != '.' else None
+        if project is not None:
+            version = get_version(project)
+            builder = ProjectBuilder(repo_name, src_dir, project,  version)
+        else:
+            builder = DocBuilder(repo_name, src_dir)
+        with builder:
+            builder.build()
+    root = Path('_build') / repo_name
+    with open(root / '.nojekyll', 'w') as fh:
+        fh.write('')
+    for project in doc_roots:
+        if project == '.':
+            continue
+        with open(f'_build/{repo_name}/{project}/index.html', 'w') as index:
+            index.write('<meta http-equiv="refresh" content="0; URL=stable/">\n')
+
+
+class DocBuilder:
+    '''Builder for the top-level documentation.
+    '''
+    build_dir = '_build/doctrees'
+
+    def __init__(self, repo_name, src_dir):
+        self.repo_name = repo_name
+        self.src_dir = src_dir
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        return False
+
+    def build(self):
+        cmd = self.command()
+        env = self.environ()
+        subprocess.check_call(cmd, env=env)
+
+    def command(self):
+        return self.cmd_prefix + self.cmd_opts + [
+            f'{self.src_dir}/docs',
+            self.dest_dir,
+        ]
+
+    def environ(self):
+        return os.environ.copy()
+
+    @property
+    def cmd_prefix(self):
+        return [
             'sphinx-build',
             '-c', 'docs',
             '-a', '-E', '-W',
-            '-A', 'root_url=/' + repo_name,
-            '-d', '_build/doctrees/' + doc_root,
+            '-A', 'root_url=/' + self.repo_name,
+            '-d', self.doc_tree,
         ]
-        if version:
-            env['PYTHONPATH'] = doc_root
-            cmd += [
-                '-D', 'release=' + version,
-                '-D', 'project=' + doc_root,
+
+    @property
+    def cmd_opts(self):
+        return []
+
+    @property
+    def dest_dir(self):
+        return f'_build/{self.repo_name}'
+
+    @property
+    def doc_tree(self):
+        return '_build/doctrees'
+
+
+class ProjectBuilder(DocBuilder):
+    '''Builder for individual projects, with project name and version.
+    '''
+    def __init__(self, repo_name, src_dir, project_name, version):
+        super().__init__(repo_name, src_dir)
+        self.project_name = project_name
+        self.version = version
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        # Remove static theme files from project static, they're
+        # used from the top-level _static.
+        for staticfile in Path(self.dest_dir).glob('_static/*'):
+            # The options are project-specific.
+            if staticfile.name != 'documentation_options.js':
+                staticfile.unlink()
+
+    def environ(self):
+        env = super().environ()
+        env['PYTHONPATH'] = self.src_dir
+        return env
+
+    @property
+    def cmd_opts(self):
+        return [
+                '-D', 'release=' + self.version,
+                '-D', 'project=' + self.project_name,
             ]
-            build_dir = '_build/' + repo_name + '/' + doc_root + '/stable'
-        else:
-            build_dir = '_build/' + repo_name
-        cmd += [
-            doc_root + '/docs',
-            build_dir,
-        ]
-        subprocess.check_call(' '.join(cmd), env=env, shell=True)
-        if build_dir.endswith('/stable'):
-            with open(build_dir.replace('/stable', '/index.html'), 'w') as index:
-                index.write('<meta http-equiv="refresh" content="0; URL=stable/">\n')
+
+    @property
+    def dest_dir(self):
+        return f'_build/{self.repo_name}/{self.project_name}/stable'

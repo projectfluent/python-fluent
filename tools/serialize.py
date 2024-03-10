9e25854fdc3527fafdcf0d9fd9fbd37c1f88e810
Eemeli Aro <eemeli@mozilla.com>
2024-03-10 12:09:09 +0200

    Move flake8 config to top level, add a separate CI action for linting

--- tools/serialize.py
+++ tools/serialize.py
@@ -2,14 +2,14 @@
 
 import sys
 import json
-
-sys.path.append('./')
 import codecs
 from fluent.syntax import ast, serialize
 
+sys.path.append("./")
+
 
 def read_json(path):
-    with codecs.open(path, 'r', encoding='utf-8') as file:
+    with codecs.open(path, "r", encoding="utf-8") as file:
         return json.load(file)
 
 
@@ -17,7 +17,8 @@ def pretty_print(fileType, data):
     resource = ast.from_json(data)
     print(serialize(resource))
 
+
 if __name__ == "__main__":
-    file_type = 'ftl'
+    file_type = "ftl"
     f = read_json(sys.argv[1])
     pretty_print(file_type, f)

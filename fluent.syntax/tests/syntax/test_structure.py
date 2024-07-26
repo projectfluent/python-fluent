df5ef402be3cf61f4a16e54e490e37e9cf276c05
James Owen <jamesowen@outlook.com>
2024-07-26 16:41:16 +0100

    Respect the `with_spans` parser option for annotations (#201)
    * Add versions of the syntax tests without spans
    
    Prior to this change, all of the syntax tests covered the behaviour of
    the parser when with_spans is set to True.
    
    This change updates the test generation to create a version of each test
    which tests the parser when with_spans is set to False. To achieve this,
    we strip the span information from the expected file (rather than
    needing to maintain two files).
    
    * Respect the with_spans setting for Annotations
    
    Prior to this change, Annotations would always have span information,
    irrespective of the value of the with_spans argument to the parser.
    
    This change changes the behaviour to only include span information if
    with_spans if set to True.

--- fluent.syntax/tests/syntax/test_structure.py
+++ fluent.syntax/tests/syntax/test_structure.py
@@ -12,23 +12,47 @@ def read_file(path):
     return text
 
 
+def without_spans(expected):
+    """
+    Given an expected JSON fragment with span information, recursively replace all of the spans
+    with None.
+    """
+    if isinstance(expected, dict):
+        result = {}
+        for key, value in expected.items():
+            if key == "span":
+                result[key] = None
+            else:
+                result[key] = without_spans(value)
+
+        return result
+    elif isinstance(expected, list):
+        return [without_spans(item) for item in expected]
+    else:
+        # We have been passed something which would not have span information in it
+        return expected
+
+
 fixtures = os.path.join(os.path.dirname(__file__), "fixtures_structure")
 
 
 class TestStructureMeta(type):
     def __new__(mcs, name, bases, attrs):
 
-        def gen_test(file_name):
+        def gen_test(file_name, with_spans):
             def test(self):
                 ftl_path = os.path.join(fixtures, file_name + ".ftl")
                 ast_path = os.path.join(fixtures, file_name + ".json")
 
                 source = read_file(ftl_path)
-                expected = read_file(ast_path)
+                expected = json.loads(read_file(ast_path))
 
-                ast = parse(source)
+                if not with_spans:
+                    expected = without_spans(expected)
 
-                self.assertEqual(ast.to_json(), json.loads(expected))
+                ast = parse(source, with_spans=with_spans)
+
+                self.assertEqual(ast.to_json(), expected)
 
             return test
 
@@ -38,8 +62,8 @@ class TestStructureMeta(type):
             if ext != ".ftl":
                 continue
 
-            test_name = f"test_{file_name}"
-            attrs[test_name] = gen_test(file_name)
+            attrs[f"test_{file_name}_with_spans"] = gen_test(file_name, with_spans=True)
+            attrs[f"test_{file_name}_without_spans"] = gen_test(file_name, with_spans=False)
 
         return type.__new__(mcs, name, bases, attrs)
 

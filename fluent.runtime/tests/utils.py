0c9c67b53eb2f992425787824ca51495dafcf5b3
studyingegret <110317665+studyingegret@users.noreply.github.com>
2025-02-07 08:13:40 +0800

    Try to fix test errors on Unix

--- fluent.runtime/tests/utils.py
+++ fluent.runtime/tests/utils.py
@@ -1,7 +1,7 @@
 """Utilities for testing."""
 
 import textwrap
-from pathlib import PurePath
+from pathlib import PureWindowsPath, PurePosixPath
 from unittest import mock
 from io import StringIO
 import functools
@@ -11,16 +11,21 @@ def dedent_ftl(text):
     return textwrap.dedent(f"{text.rstrip()}\n")
 
 
-# Unify path separator, default path separator on Windows is \ not /
-# Supports only relative paths
 # Needed in test_falllback.py because it uses dict + string compare to make a virtual file structure
-def _normalize_path(path):
+def _normalize_file_path(path):
     """Note: Does not support absolute paths or paths that
     contain '.' or '..' parts."""
-    path = PurePath(path)
-    if path.is_absolute() or "." in path.parts or ".." in path.parts:
+    # Cannot use os.path or PurePath, because they only recognize
+    # one kind of path separator
+    if PureWindowsPath(path).is_absolute() or PurePosixPath(path).is_absolute():
         raise ValueError(f"Unsupported path: {path}")
-    return "/".join(path.parts)
+    parts = path.replace("\\", "/").split("/")
+    if "." in parts or ".." in parts:
+        # path ends with a trailing pathsep
+        raise ValueError(f"Unsupported path: {path}")
+    if parts and parts[-1] == "":
+        raise ValueError(f"Path appears to be a directory, not a file: {path}")
+    return "/".join(parts)
 
 
 def patch_files(files: dict):
@@ -32,12 +37,12 @@ def patch_files(files: dict):
 
     The implementation may be changed to match the mechanism used.
     """
-    if files is None:
-        files = {}
+
+    # Here it is possible to validate file names, but skipped
 
     def then(func):
-        @mock.patch("os.path.isfile", side_effect=lambda p: _normalize_path(p) in files)
-        @mock.patch("codecs.open", side_effect=lambda p, _, __: StringIO(files[_normalize_path(p)]))
+        @mock.patch("os.path.isfile", side_effect=lambda p: _normalize_file_path(p) in files)
+        @mock.patch("codecs.open", side_effect=lambda p, _, __: StringIO(files[_normalize_file_path(p)]))
         @functools.wraps(func)  # Make ret look like func to later decorators
         def ret(*args, **kwargs):
             func(*args[:-2], **kwargs)
